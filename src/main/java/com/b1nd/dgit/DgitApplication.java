8e81cc3e3450fa52356e3a02cbf1d296c4bc8bca
krung2 <jungbin4337@dgsw.hs.kr>
2021-10-26 17:04:55 +0900

    Initalize

---
+++ src/main/java/com/b1nd/dgit/DgitApplication.java
@@ -0,0 +1,13 @@
+package com.b1nd.dgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DgitApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DgitApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/b1nd/dgit/DgitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.b1nd.dgit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DgitApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}

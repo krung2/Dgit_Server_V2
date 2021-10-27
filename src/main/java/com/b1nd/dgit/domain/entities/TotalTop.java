7546d8981da2344b8aec152b1d411825dbcd0d43
krung2 <jungbin4337@dgsw.hs.kr>
2021-10-27 15:48:19 +0900

    Create TotalTop.java

---
+++ src/main/java/com/b1nd/dgit/domain/entities/TotalTop.java
@@ -0,0 +1,26 @@
+package com.b1nd.dgit.domain.entities;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "total_top")
+@Getter
+@NoArgsConstructor
+public class TotalTop {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "idx", unique = true, nullable = false)
+  private Long idx;
+
+  @OneToOne(fetch = FetchType.EAGER)
+  @JoinColumn(name = "fk_user_id")
+  private User user;
+
+  @Column(name = "date", nullable = false)
+  private Date date;
+}

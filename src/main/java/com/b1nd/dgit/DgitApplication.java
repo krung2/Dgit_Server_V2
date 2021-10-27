e4e0af00eedfbf93cdcd6e8d817bce6487306281
krung2 <jungbin4337@dgsw.hs.kr>
2021-10-27 15:48:48 +0900

    ADD enum & JpaAuditing

--- src/main/java/com/b1nd/dgit/DgitApplication.java
+++ src/main/java/com/b1nd/dgit/DgitApplication.java
@@ -2,7 +2,9 @@ package com.b1nd.dgit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class DgitApplication {
 
---
+++ src/main/java/com/b1nd/dgit/enums/week/WeekDay.java
@@ -0,0 +1,11 @@
+package com.b1nd.dgit.enums.week;
+
+public enum WeekDay {
+  SUNDAY,
+  MONDAY,
+  TUESDAY,
+  WEDNESDAY,
+  THURSDAY,
+  FRIDAY,
+  SATURDAY
+}

7fc427b1a8f7b887c9276d2b0798c0a592d7f2d8
krung2 <jungbin4337@dgsw.hs.kr>
2021-10-27 15:48:24 +0900

    Create WeeklyTop.java

---
+++ src/main/java/com/b1nd/dgit/domain/entities/WeeklyTop.java
@@ -0,0 +1,29 @@
+package com.b1nd.dgit.domain.entities;
+
+import com.b1nd.dgit.enums.week.WeekDay;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "weekly_top")
+@NoArgsConstructor
+public class WeeklyTop extends BaseEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "idx", unique = true, nullable = false)
+  private Long idx;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "fk_user_id")
+  private User user;
+
+  @Column(name = "contribute", nullable = false)
+  private Long contribute;
+
+  @Column(name = "date", nullable = false)
+  private Date date;
+}

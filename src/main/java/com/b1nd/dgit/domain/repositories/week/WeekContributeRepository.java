3ec77a8135944496d85d0e4f1b20c654f59bf53d
krung2 <jungbin4337@dgsw.hs.kr>
2021-10-30 19:25:46 +0900

    ADD weekContributeRepository

---
+++ src/main/java/com/b1nd/dgit/domain/repositories/week/WeekContributeRepository.java
@@ -0,0 +1,7 @@
+package com.b1nd.dgit.domain.repositories.week;
+
+import com.b1nd.dgit.domain.entities.WeekContribute;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WeekContributeRepository extends JpaRepository<WeekContribute, Long> {
+}

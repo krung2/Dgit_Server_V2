0829594426f5e7293badc7d7878df90123efa92f
krung2 <jungbin4337@dgsw.hs.kr>
2021-10-30 19:24:57 +0900

    ADD WeeklyTopRepository

---
+++ src/main/java/com/b1nd/dgit/domain/repositories/week/WeeklyTopRepository.java
@@ -0,0 +1,7 @@
+package com.b1nd.dgit.domain.repositories.week;
+
+import com.b1nd.dgit.domain.entities.WeeklyTop;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WeeklyTopRepository extends JpaRepository<WeeklyTop, Long> {
+}

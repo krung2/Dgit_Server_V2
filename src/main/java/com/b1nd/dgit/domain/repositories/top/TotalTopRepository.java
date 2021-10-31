2cc3d8490f3fd846e9958946e2781b5c8b67e362
krung2 <jungbin4337@dgsw.hs.kr>
2021-10-31 19:28:29 +0900

    ADD totalTopRepository

---
+++ src/main/java/com/b1nd/dgit/domain/repositories/top/TotalTopRepository.java
@@ -0,0 +1,7 @@
+package com.b1nd.dgit.domain.repositories.top;
+
+import com.b1nd.dgit.domain.entities.TotalTop;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TotalTopRepository extends JpaRepository<TotalTop, Long> {
+}

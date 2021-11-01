596a8b7603ffb24df4811bae6a434399f8320ac1
krung2 <jungbin4337@dgsw.hs.kr>
2021-11-01 22:21:54 +0900

    ADD bearer authentication

--- src/main/java/com/b1nd/dgit/config/SwaggerConfig.java
+++ src/main/java/com/b1nd/dgit/config/SwaggerConfig.java
@@ -6,8 +6,15 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -15,12 +22,32 @@ public class SwaggerConfig {
   @Bean
   public Docket swaggerAPI () {
     return new Docket(DocumentationType.SWAGGER_2)
+            .securityContexts(Arrays.asList(securityContext()))
+            .securitySchemes(Arrays.asList(apiKey()))
             .useDefaultResponseMessages(false)
             .select()
             .apis(RequestHandlerSelectors.any())
             .paths(PathSelectors.any())
             .build()
             .apiInfo(swaggerInfo());
+  }
+
+  private SecurityContext securityContext() {
+    return SecurityContext.builder()
+            .securityReferences(defaultAuth())
+            .build();
+  }
+
+  private List<SecurityReference> defaultAuth() {
+    AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+    AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+    authorizationScopes[0] = authorizationScope;
+    return Arrays.asList(new SecurityReference("Authorization", authorizationScopes));
+  }
+
+
+  private ApiKey apiKey() {
+    return new ApiKey("Authorization", "Authorization", "header");
   }
 
   private ApiInfo swaggerInfo() {

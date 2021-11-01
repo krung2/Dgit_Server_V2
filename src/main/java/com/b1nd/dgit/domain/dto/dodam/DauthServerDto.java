28c715f80ef9ad2b7f75cedf70c641611e632828
krung2 <jungbin4337@dgsw.hs.kr>
2021-11-01 17:17:22 +0900

    FIX dauthResponse dto

--- src/main/java/com/b1nd/dgit/domain/dto/dodam/DauthServerDto.java
+++ src/main/java/com/b1nd/dgit/domain/dto/dodam/DauthServerDto.java
@@ -2,23 +2,14 @@ package com.b1nd.dgit.domain.dto.dodam;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.ToString;
 
 @Getter
-public class DauthServerDto extends RestBaseDto {
+@AllArgsConstructor
+public class DauthServerDto  {
 
-  private final DauthServerData data;
-
-  public DauthServerDto (int status, String message, DauthServerData data) {
-    super(status, message);
-    this.data = data;
-  }
-
-  @Getter
-  @AllArgsConstructor
-  public static class DauthServerData {
-    private String access_token;
-    private String refresh_token;
-    private String token_type;
-    private String expires_in;
-  }
+  private String access_token;
+  private String refresh_token;
+  private String token_type;
+  private String expires_in;
 }

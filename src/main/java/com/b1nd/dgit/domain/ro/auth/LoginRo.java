c30f44d98e269762503bd0ae550a1a5563c6d684
krung2 <jungbin4337@dgsw.hs.kr>
2021-11-01 16:46:18 +0900

    ADD login Ro builder pattern

--- src/main/java/com/b1nd/dgit/domain/ro/auth/LoginRo.java
+++ src/main/java/com/b1nd/dgit/domain/ro/auth/LoginRo.java
@@ -1,16 +1,22 @@
 package com.b1nd.dgit.domain.ro.auth;
 
 import com.b1nd.dgit.domain.entities.User;
-import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-@AllArgsConstructor
 public class LoginRo {
 
   private User user;
   private String token;
   private String refreshToken;
+
+  @Builder
+  public LoginRo (User user, String token, String refreshToken) {
+    this.user = user;
+    this.token = token;
+    this.refreshToken = refreshToken;
+  }
 }

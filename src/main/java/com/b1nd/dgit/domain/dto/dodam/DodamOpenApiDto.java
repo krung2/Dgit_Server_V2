308f0aa6e2382c2cc6ffa3eb43ea62de82453f16
krung2 <jungbin4337@dgsw.hs.kr>
2021-11-01 12:29:19 +0900

    ADD dodamOpenApiDto

---
+++ src/main/java/com/b1nd/dgit/domain/dto/dodam/DodamOpenApiDto.java
@@ -0,0 +1,40 @@
+package com.b1nd.dgit.domain.dto.dodam;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+public class DodamOpenApiDto extends RestBaseDto {
+
+  private final DodamInfoData data;
+
+  public DodamOpenApiDto (int status, String message, DodamInfoData data) {
+    super(status, message);
+    this.data = new DodamInfoData(data);
+  }
+
+  @Getter
+  @AllArgsConstructor
+  public static class DodamInfoData {
+
+    private final String uniqueId;
+    private final int grade;
+    private final int room;
+    private final int number;
+    private final String name;
+    private final String email;
+    private final String profileImage;
+    private final int accessLevel;
+
+    public DodamInfoData (DodamInfoData data) {
+      this.uniqueId = data.getUniqueId();
+      this.grade = data.getGrade();
+      this.room = data.getRoom();
+      this.number = data.getNumber();
+      this.name = data.getName();
+      this.email = data.getEmail();
+      this.profileImage = data.getProfileImage();
+      this.accessLevel = data.getAccessLevel();
+    }
+  }
+}

4eaad95b0d656aeb6a9eaa9b66bd0a189371be0f
krung2 <jungbin4337@dgsw.hs.kr>
2021-11-01 16:46:05 +0900

    ADD login router

--- src/main/java/com/b1nd/dgit/controller/AuthController.java
+++ src/main/java/com/b1nd/dgit/controller/AuthController.java
@@ -1,12 +1,32 @@
 package com.b1nd.dgit.controller;
 
+import com.b1nd.dgit.domain.dto.auth.DodamLoginDto;
+import com.b1nd.dgit.domain.model.http.ResponseData;
+import com.b1nd.dgit.domain.ro.auth.LoginRo;
 import com.b1nd.dgit.service.auth.AuthService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 @RestController
+@RequestMapping(value = "/auth")
 @RequiredArgsConstructor
 public class AuthController {
 
   private final AuthService authServiceImpl;
+
+  @PostMapping(value = "/login")
+  public ResponseEntity<ResponseData<LoginRo>> login (final @Valid @RequestBody DodamLoginDto dodamLoginDto) {
+    return ResponseData.res(
+            HttpStatus.OK,
+            "로그인 성공",
+            this.authServiceImpl.dodamLogin(dodamLoginDto)
+    );
+  }
 }

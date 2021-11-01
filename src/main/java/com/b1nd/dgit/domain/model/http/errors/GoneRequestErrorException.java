983e5033ad2e65c7ed30b66ade866b07cd3e398e
krung2 <jungbin4337@dgsw.hs.kr>
2021-11-01 12:28:36 +0900

    ADD GoneErrorException

---
+++ src/main/java/com/b1nd/dgit/domain/model/http/errors/GoneRequestErrorException.java
@@ -0,0 +1,12 @@
+package com.b1nd.dgit.domain.model.http.errors;
+
+public class GoneRequestErrorException extends CustomError{
+
+  public GoneRequestErrorException(ErrorCodes errorCode) {
+    super(errorCode);
+  }
+
+  public static CustomError of (String message) {
+    return new CustomError(message, ErrorCodes.RESOURCE_NOT_AVAILABLE);
+  }
+}

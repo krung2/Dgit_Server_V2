e92ec7d02afde358148b758babf794955f57a332
krung2 <jungbin4337@dgsw.hs.kr>
2021-11-01 12:28:20 +0900

    ADD get UserById

--- src/main/java/com/b1nd/dgit/domain/repositories/user/UserRepository.java
+++ src/main/java/com/b1nd/dgit/domain/repositories/user/UserRepository.java
@@ -1,9 +1,12 @@
 package com.b1nd.dgit.domain.repositories.user;
 
 import com.b1nd.dgit.domain.entities.User;
+import com.sun.istack.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, String> {
 
-
+  Optional<User> findById(String id);
 }

75dfab9054b8256d1bd33256c5145273d628518c
krung2 <jungbin4337@dgsw.hs.kr>
2021-11-02 19:36:45 +0900

    ADD fina all query

--- src/main/java/com/b1nd/dgit/domain/repositories/user/GithubUserRepository.java
+++ src/main/java/com/b1nd/dgit/domain/repositories/user/GithubUserRepository.java
@@ -1,7 +1,12 @@
 package com.b1nd.dgit.domain.repositories.user;
 
 import com.b1nd.dgit.domain.entities.GithubUser;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface GithubUserRepository extends JpaRepository<GithubUser, String> {
+
+  List<GithubUser> findAll (Sort sort);
 }

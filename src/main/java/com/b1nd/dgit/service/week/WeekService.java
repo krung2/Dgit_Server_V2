9a648092c85cd1ac6475b48dcfa76a2882fc8511
krung2 <jungbin4337@dgsw.hs.kr>
2021-11-02 23:45:34 +0900

    ADD get all data

--- src/main/java/com/b1nd/dgit/service/week/WeekService.java
+++ src/main/java/com/b1nd/dgit/service/week/WeekService.java
@@ -1,13 +1,14 @@
 package com.b1nd.dgit.service.week;
 
-import com.b1nd.dgit.domain.dto.week.WeekSaveDto;
 import com.b1nd.dgit.domain.entities.GithubUser;
 import com.b1nd.dgit.domain.entities.WeekContribute;
 import github.queries.GetContributionQuery;
 
-import java.text.ParseException;
+import java.util.List;
 
 public interface WeekService {
 
   WeekContribute save(GithubUser githubUser, GetContributionQuery.ContributionDay contributionData);
+  void deleteAllData ();
+  List<WeekContribute> getAllData ();
 }
--- src/main/java/com/b1nd/dgit/service/week/WeekServiceImpl.java
+++ src/main/java/com/b1nd/dgit/service/week/WeekServiceImpl.java
@@ -3,15 +3,21 @@ package com.b1nd.dgit.service.week;
 import com.b1nd.dgit.domain.entities.GithubUser;
 import com.b1nd.dgit.domain.entities.WeekContribute;
 import com.b1nd.dgit.domain.repositories.week.WeekContributeRepository;
+import com.b1nd.dgit.domain.ro.week.WeeklyRankRo;
 import com.b1nd.dgit.enums.week.WeekDay;
+import com.b1nd.dgit.service.githubUser.GithubUserService;
 import github.queries.GetContributionQuery;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class WeekServiceImpl implements WeekService{
 
+  private final GithubUserService githubUserService;
   private final WeekContributeRepository weekContributeRepository;
 
   public WeekContribute save(GithubUser githubUser, GetContributionQuery.ContributionDay contributionData) {
@@ -22,5 +28,41 @@ public class WeekServiceImpl implements WeekService{
             .weekDay(WeekDay.values()[contributionData.weekday()])
             .build()
     );
+  }
+
+  public void deleteAllData () {
+    weekContributeRepository.deleteAll();
+  }
+
+  public List<WeekContribute> getAllData () {
+    return weekContributeRepository.getAll();
+  }
+
+  public List<WeeklyRankRo> getWeeklyRanking () {
+
+    Map<GithubUser, Integer> userToCommit = new HashMap<>();
+
+    githubUserService.githubUserList().forEach(githubUser -> userToCommit.put(githubUser, 0));
+
+    getAllData().forEach(weekContribute -> {
+      if(!userToCommit.containsKey(weekContribute.getGithubUser())) {
+        userToCommit.put(
+                weekContribute.getGithubUser(),
+                weekContribute.getContribute()
+        );
+      } else {
+        userToCommit.put(
+                weekContribute.getGithubUser(),
+                userToCommit.get(weekContribute.getGithubUser()) + weekContribute.getContribute()
+        );
+      }
+    });
+
+    WeeklyRankRo[] weeklyRanks = userToCommit.entrySet().stream()
+            .map(data -> new WeeklyRankRo(data.getKey(), data.getValue())).toArray(WeeklyRankRo[]::new);
+
+    return Arrays.stream(weeklyRanks)
+            .sorted(Comparator.comparing(WeeklyRankRo::getWeeklyCommit).reversed())
+            .collect(Collectors.toList());
   }
 }

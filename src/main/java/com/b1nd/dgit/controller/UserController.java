d37974bb9ef2eaef3c6e98e66bc85ffa087ebba8
krung2 <jungbin4337@dgsw.hs.kr>
2021-11-02 17:13:38 +0900

    ADD get my info

--- src/main/java/com/b1nd/dgit/controller/UserController.java
+++ src/main/java/com/b1nd/dgit/controller/UserController.java
@@ -4,6 +4,7 @@ import com.b1nd.dgit.annotation.UserLoginToken;
 import com.b1nd.dgit.domain.dto.user.ModifyGithubDto;
 import com.b1nd.dgit.domain.entities.User;
 import com.b1nd.dgit.domain.model.http.Response;
+import com.b1nd.dgit.domain.model.http.ResponseData;
 import com.b1nd.dgit.service.user.UserService;
 import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
@@ -26,7 +27,13 @@ public class UserController {
           final @RequestAttribute User user,
           final @Valid @RequestBody ModifyGithubDto modifyGithubDto
   ) {
-    userServiceImpl.modifyGithubId(user.getId(), modifyGithubDto);
+    userServiceImpl.modifyGithubId(user, modifyGithubDto);
     return Response.ok("깃허브 수정 성공");
+  }
+
+  @UserLoginToken
+  @GetMapping("/")
+  public ResponseEntity<ResponseData<User>> getUser (@RequestAttribute User user) {
+    return ResponseData.ok("자신의 정보 가져오기", userServiceImpl.findById(user.getId()));
   }
 }

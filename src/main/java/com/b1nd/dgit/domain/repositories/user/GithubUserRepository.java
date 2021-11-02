b540a94d48a8f053d0a23c91ab25c1889257acf9
krung2 <jungbin4337@dgsw.hs.kr>
2021-11-02 17:14:36 +0900

    FIX pk type

--- src/main/java/com/b1nd/dgit/domain/repositories/user/GithubUserRepository.java
+++ src/main/java/com/b1nd/dgit/domain/repositories/user/GithubUserRepository.java
@@ -3,5 +3,5 @@ package com.b1nd.dgit.domain.repositories.user;
 import com.b1nd.dgit.domain.entities.GithubUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface GithubUserRepository extends JpaRepository<GithubUser, Long> {
+public interface GithubUserRepository extends JpaRepository<GithubUser, String> {
 }

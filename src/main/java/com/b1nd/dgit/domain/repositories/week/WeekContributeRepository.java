ecaf007d29292b80ac875e14def3eafef127a484
krung2 <jungbin4337@dgsw.hs.kr>
2021-11-02 23:45:22 +0900

    ADD methods week contribute repo

--- src/main/java/com/b1nd/dgit/domain/repositories/week/WeekContributeRepository.java
+++ src/main/java/com/b1nd/dgit/domain/repositories/week/WeekContributeRepository.java
@@ -3,6 +3,10 @@ package com.b1nd.dgit.domain.repositories.week;
 import com.b1nd.dgit.domain.entities.WeekContribute;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface WeekContributeRepository extends JpaRepository<WeekContribute, Long> {
 
+  void deleteAll ();
+  List<WeekContribute> getAll ();
 }

626492eb2310c73d160c8031d73e9acfecd7f38c
krung2 <jungbin4337@dgsw.hs.kr>
2021-11-02 21:33:16 +0900

    FIX save week contribute

--- src/main/java/com/b1nd/dgit/service/week/WeekService.java
+++ src/main/java/com/b1nd/dgit/service/week/WeekService.java
@@ -1,5 +1,13 @@
 package com.b1nd.dgit.service.week;
 
+import com.b1nd.dgit.domain.dto.week.WeekSaveDto;
+import com.b1nd.dgit.domain.entities.GithubUser;
+import com.b1nd.dgit.domain.entities.WeekContribute;
+import github.queries.GetContributionQuery;
+
+import java.text.ParseException;
+
 public interface WeekService {
 
+  WeekContribute save(GithubUser githubUser, GetContributionQuery.ContributionDay contributionData);
 }
--- src/main/java/com/b1nd/dgit/service/week/WeekServiceImpl.java
+++ src/main/java/com/b1nd/dgit/service/week/WeekServiceImpl.java
@@ -1,9 +1,9 @@
 package com.b1nd.dgit.service.week;
 
-import com.b1nd.dgit.domain.dto.week.WeekSaveDto;
 import com.b1nd.dgit.domain.entities.GithubUser;
 import com.b1nd.dgit.domain.entities.WeekContribute;
 import com.b1nd.dgit.domain.repositories.week.WeekContributeRepository;
+import com.b1nd.dgit.enums.week.WeekDay;
 import github.queries.GetContributionQuery;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,12 +14,12 @@ public class WeekServiceImpl implements WeekService{
 
   private final WeekContributeRepository weekContributeRepository;
 
-  public WeekContribute save(GithubUser githubUser, WeekSaveDto weekSaveDto) {
+  public WeekContribute save(GithubUser githubUser, GetContributionQuery.ContributionDay contributionData) {
     return weekContributeRepository.save(WeekContribute.builder()
             .githubUser(githubUser)
-            .contribute(weekSaveDto.getContribute())
-            .date(weekSaveDto.getDate())
-            .weekDay(weekSaveDto.getWeekDay())
+            .contribute(contributionData.contributionCount())
+            .date(contributionData.date())
+            .weekDay(WeekDay.values()[contributionData.weekday()])
             .build()
     );
   }

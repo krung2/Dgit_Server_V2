ceb53ba62dd5038529909933c7f3c00356b61ee1
krung2 <jungbin4337@dgsw.hs.kr>
2021-11-02 23:45:05 +0900

    ADD weeklyRank ro

---
+++ src/main/java/com/b1nd/dgit/domain/ro/week/WeeklyRankRo.java
@@ -0,0 +1,20 @@
+package com.b1nd.dgit.domain.ro.week;
+
+import com.b1nd.dgit.domain.entities.GithubUser;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class WeeklyRankRo {
+
+  private GithubUser githubUser;
+  private int weeklyCommit;
+
+  @Builder
+  public WeeklyRankRo (GithubUser githubUser, int weeklyCommit) {
+    this.githubUser = githubUser;
+    this.weeklyCommit = weeklyCommit;
+  }
+}

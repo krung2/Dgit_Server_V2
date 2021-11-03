b906cabd26c3210545e10fa0c266a78c5b428511
krung2 <jungbin4337@dgsw.hs.kr>
2021-11-03 13:32:35 +0900

    ADD total rank ro

---
+++ src/main/java/com/b1nd/dgit/domain/ro/user/TotalRankRo.java
@@ -0,0 +1,22 @@
+package com.b1nd.dgit.domain.ro.user;
+
+import com.b1nd.dgit.domain.entities.GithubUser;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class TotalRankRo {
+
+  private List<GithubUser> users;
+  private int totalTop;
+
+  @Builder
+  public TotalRankRo (List<GithubUser> users, int totalTop) {
+    this.users = users;
+    this.totalTop = totalTop;
+  }
+}

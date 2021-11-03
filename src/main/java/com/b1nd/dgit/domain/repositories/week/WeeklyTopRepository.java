3a5a9dbc9e9c940ff0f3e3f33d266cbf7e888729
krung2 <jungbin4337@dgsw.hs.kr>
2021-11-03 16:34:00 +0900

    ADD weeklyTop entity graph

--- src/main/java/com/b1nd/dgit/domain/repositories/week/WeeklyTopRepository.java
+++ src/main/java/com/b1nd/dgit/domain/repositories/week/WeeklyTopRepository.java
@@ -1,8 +1,17 @@
 package com.b1nd.dgit.domain.repositories.week;
 
+import com.b1nd.dgit.domain.entities.TotalTop;
 import com.b1nd.dgit.domain.entities.WeeklyTop;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface WeeklyTopRepository extends JpaRepository<WeeklyTop, Long> {
 
+  @EntityGraph(attributePaths = "githubUser")
+  @Query("select weeklyTop from WeeklyTop weeklyTop")
+  List<WeeklyTop> findEntityGraph (Sort sort);
 }

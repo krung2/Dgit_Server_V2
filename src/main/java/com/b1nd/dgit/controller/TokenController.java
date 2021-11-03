758de0a5c940974018eec69278b5a27700340a2a
krung2 <jungbin4337@dgsw.hs.kr>
2021-11-03 13:31:25 +0900

    FIX token prefix

--- src/main/java/com/b1nd/dgit/controller/TokenController.java
+++ src/main/java/com/b1nd/dgit/controller/TokenController.java
@@ -8,11 +8,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import javax.validation.Valid;
 
 @Api(tags = {"Token"})
-@RestController(value = "/token")
+@RestController
+@RequestMapping(value = "/token")
 @RequiredArgsConstructor
 public class TokenController {
 

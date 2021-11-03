02479e664054025e35b74c8ce95c49646667b5a2
krung2 <jungbin4337@dgsw.hs.kr>
2021-11-03 10:18:55 +0900

    FIX find all to graph

--- src/main/java/com/b1nd/dgit/domain/repositories/user/GithubUserRepository.java
+++ src/main/java/com/b1nd/dgit/domain/repositories/user/GithubUserRepository.java
@@ -2,11 +2,23 @@ package com.b1nd.dgit.domain.repositories.user;
 
 import com.b1nd.dgit.domain.entities.GithubUser;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface GithubUserRepository extends JpaRepository<GithubUser, String> {
 
-  List<GithubUser> findAll (Sort sort);
+  @EntityGraph(attributePaths = "user")
+  @Query("select githubUser from GithubUser githubUser")
+  List<GithubUser> findEntityGraph ();
+
+  @EntityGraph(attributePaths = "user")
+  @Query("select githubUser from GithubUser githubUser")
+  List<GithubUser> findEntityGraph (Sort sort);
+
+  @Override
+  Optional<GithubUser> findById(String githubId);
 }

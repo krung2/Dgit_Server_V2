b256710e53dcc703cc4b7281a6ba682390d89d56
krung2 <jungbin4337@dgsw.hs.kr>
2021-11-03 11:14:40 +0900

    FIX scheduler get contribute

--- src/main/java/com/b1nd/dgit/service/schedule/ScheduleServiceImpl.java
+++ src/main/java/com/b1nd/dgit/service/schedule/ScheduleServiceImpl.java
@@ -8,7 +8,9 @@ import github.queries.GetContributionQuery;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -19,12 +21,14 @@ public class ScheduleServiceImpl implements ScheduleService {
   private final GithubService githubService;
 
   public void weekContributeCheck() {
+    weekServiceImpl.deleteAllData();
+
     githubUserService.githubUserList().forEach(githubUser -> {
       GetContributionQuery.User githubData = githubService.getData(githubUser.getGithubId()).getData().user();
       List<GetContributionQuery.Week> weekCommit = githubData.contributionsCollection().contributionCalendar().weeks();
       List<GetContributionQuery.ContributionDay> contributeDays = weekCommit.get(weekCommit.size() - 1).contributionDays();
 
-      GithubUser updateUser = githubUserService.update(githubData);
+      GithubUser updateUser = githubUserService.update(githubUser, githubData);
       contributeDays.forEach(contributeData -> weekServiceImpl.save(updateUser, contributeData));
     });
   }

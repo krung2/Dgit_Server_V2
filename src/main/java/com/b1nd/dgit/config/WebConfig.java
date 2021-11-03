30d0c89e88cc5afa49a13c9364af33dcd44af935
krung2 <jungbin4337@dgsw.hs.kr>
2021-11-03 15:10:29 +0900

    ADD webconfig

---
+++ src/main/java/com/b1nd/dgit/config/WebConfig.java
@@ -0,0 +1,16 @@
+package com.b1nd.dgit.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**").allowedMethods("*");
+  }
+}

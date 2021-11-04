d0e0f506e67b84219f794dacd8bd1b82c563d20c
krung2 <jungbin4337@dgsw.hs.kr>
2021-11-04 12:25:37 +0900

    FIX dto lombook lib

--- src/main/java/com/b1nd/dgit/domain/dto/token/RefreshTokenDto.java
+++ src/main/java/com/b1nd/dgit/domain/dto/token/RefreshTokenDto.java
@@ -1,11 +1,20 @@
 package com.b1nd.dgit.domain.dto.token;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
 
 @Getter
-@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PACKAGE)
 public class RefreshTokenDto {
 
+  @NotBlank(message = "githubIdsms 필수 입력값입니다")
   private String refreshToken;
+
+  public RefreshTokenDto (String refreshToken) {
+    this.refreshToken = refreshToken;
+  }
 }

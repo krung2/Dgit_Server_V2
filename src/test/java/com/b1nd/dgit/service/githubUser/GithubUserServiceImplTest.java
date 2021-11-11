74d7a14cabaf11fb2c2cd1530c508246b91e8bc5
krung2 <jungbin4337@dgsw.hs.kr>
2021-11-11 20:22:19 +0900

    ADD githubUserListTest Success

--- src/test/java/com/b1nd/dgit/service/githubUser/GithubUserServiceImplTest.java
+++ src/test/java/com/b1nd/dgit/service/githubUser/GithubUserServiceImplTest.java
@@ -3,18 +3,15 @@ package com.b1nd.dgit.service.githubUser;
 import com.b1nd.dgit.domain.entities.GithubUser;
 import com.b1nd.dgit.domain.entities.User;
 import com.b1nd.dgit.domain.repositories.user.GithubUserRepository;
-import github.queries.GetContributionQuery;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.Optional;
+import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
 
@@ -51,13 +48,22 @@ class GithubUserServiceImplTest {
   }
 
   @Test
-  void githubUserList() {
+  void getGithubUserList_Success() {
     // given
+    given(githubUserRepository.findEntityGraph())
+            .willReturn(List.of(mockGithubUser, mockGithubUser));
 
     // when
+    List<GithubUser> githubUserList = githubUserService.getGithubUserList();
 
     // then
-
+    assertEquals("krung2", githubUserList.get(0).getGithubId());
+    assertEquals(mockUser, githubUserList.get(0).getUser());
+    assertEquals(mockUser, githubUserList.get(1).getUser());
+    assertEquals(123, githubUserList.get(1).getTotalContributions());
+    assertEquals("", githubUserList.get(0).getUserImage());
+    assertEquals("안농!", githubUserList.get(1).getBio());
+    assertThrows(IndexOutOfBoundsException.class, () -> githubUserList.get(3));
   }
 
   @Test

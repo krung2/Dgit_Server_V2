550e6e9be90f9f33083d237cb1830430c91754d2
krung2 <jungbin4337@dgsw.hs.kr>
2021-11-11 19:25:36 +0900

    ADD existUserTest Failed

--- src/test/java/com/b1nd/dgit/service/githubUser/GithubUserServiceImplTest.java
+++ src/test/java/com/b1nd/dgit/service/githubUser/GithubUserServiceImplTest.java
@@ -3,15 +3,19 @@ package com.b1nd.dgit.service.githubUser;
 import com.b1nd.dgit.domain.entities.GithubUser;
 import com.b1nd.dgit.domain.repositories.user.GithubUserRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
 
+@ExtendWith(MockitoExtension.class)
 class GithubUserServiceImplTest {
 
   @Mock
@@ -21,13 +25,22 @@ class GithubUserServiceImplTest {
   private GithubUserServiceImpl githubUserService;
 
   @Test
-  void existUserTest() {
-    given(githubUserRepository.findById(anyString()))
-            .willReturn(Optional.of(new GithubUser()));
+  void existUserTest_Success() {
+    given(githubUserRepository.existsById(anyString()))
+            .willReturn(true);
 
     boolean test = githubUserService.existUser("123");
 
     assertTrue(test);
   }
 
+  @Test
+  void existUserTest_Failed() {
+    given(githubUserRepository.existsById(anyString()))
+            .willReturn(false);
+
+    boolean test = githubUserService.existUser("123");
+
+    assertFalse(test);
+  }
 }
